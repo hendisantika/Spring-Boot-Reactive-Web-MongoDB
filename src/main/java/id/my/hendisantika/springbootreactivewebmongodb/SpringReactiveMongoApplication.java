33ed772da45cd32e1b469df20160cb51104f62b4
hendisantika <hendisantika@yahoo.co.id>
2024-04-05 10:24:56 +0700

    Add Init Data🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootreactivewebmongodb/SpringReactiveMongoApplication.java
+++ src/main/java/id/my/hendisantika/springbootreactivewebmongodb/SpringReactiveMongoApplication.java
@@ -1,9 +1,16 @@
 package id.my.hendisantika.springbootreactivewebmongodb;
 
+import id.my.hendisantika.springbootreactivewebmongodb.model.Car;
+import id.my.hendisantika.springbootreactivewebmongodb.repository.CarRepository;
+import lombok.extern.slf4j.Slf4j;
 import lombok.val;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,13 +23,29 @@ import org.springframework.context.ConfigurableApplicationContext;
  * To change this template use File | Settings | File Templates.
  */
 @SpringBootApplication
+@Slf4j
 public class SpringReactiveMongoApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext app = SpringApplication.run(SpringReactiveMongoApplication.class, args);
 
         val env = app.getEnvironment();
-        System.out.println(env.getProperty("spring.data.mongodb.uri"));
+        log.info(env.getProperty("spring.data.mongodb.uri"));
     }
+
+    @Bean
+    public CommandLineRunner initData(CarRepository carRepository) {
+        carRepository.deleteAll();
+        return (args ->
+                carRepository.saveAll(
+                        List.of(
+                                new Car("Civic", "Honda"),
+                                new Car("Expander", "Mithsubishi"),
+                                new Car("Livina", "Nissan"),
+                                new Car("Innova", "Toyoya"),
+                                new Car("Avanza", "Toyoya")
+                        )));
+    }
+
 
 }

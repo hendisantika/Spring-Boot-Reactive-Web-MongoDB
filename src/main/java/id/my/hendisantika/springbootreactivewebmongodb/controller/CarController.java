182a248804c2d82b30c19918de9981ed10fe04d1
hendisantika <hendisantika@yahoo.co.id>
2024-04-05 09:03:52 +0700

    Add CarController 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootreactivewebmongodb/controller/CarController.java
@@ -0,0 +1,24 @@
+package id.my.hendisantika.springbootreactivewebmongodb.controller;
+
+import id.my.hendisantika.springbootreactivewebmongodb.service.CarService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Spring-Boot-Reactive-Web-MongoDB
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/5/24
+ * Time: 09:03
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping("/cars")
+@RequiredArgsConstructor
+public class CarController {
+    private final CarService service;
+
+}

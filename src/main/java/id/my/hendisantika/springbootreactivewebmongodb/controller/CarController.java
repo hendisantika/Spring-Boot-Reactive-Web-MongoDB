dad161f1685fe6b15bf0fc8ac92c0bc1d1fbabd4
hendisantika <hendisantika@yahoo.co.id>
2024-04-05 09:04:18 +0700

    Add byId in CarController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootreactivewebmongodb/controller/CarController.java
+++ src/main/java/id/my/hendisantika/springbootreactivewebmongodb/controller/CarController.java
@@ -3,11 +3,14 @@ package id.my.hendisantika.springbootreactivewebmongodb.controller;
 import id.my.hendisantika.springbootreactivewebmongodb.model.Car;
 import id.my.hendisantika.springbootreactivewebmongodb.service.CarService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 /**
  * Created by IntelliJ IDEA.
@@ -29,6 +32,11 @@ public class CarController {
     public Flux<Car> all(@RequestParam(required = false) String brand) {
         if (brand == null) return service.all();
         return service.byBrand(brand);
+    }
+
+    @GetMapping("/{id}")
+    public Mono<ResponseEntity<?>> byId(@PathVariable String id) {
+        return service.createCarMonoOpt(service.byId(id)).flatMap(flatMapMonoCarOK);
     }
 
 }

8d792d0bbacc4568a027abf9da4c02e47c67d0e8
hendisantika <hendisantika@yahoo.co.id>
2024-04-05 09:01:47 +0700

    Add CarService 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootreactivewebmongodb/service/CarService.java
@@ -0,0 +1,23 @@
+package id.my.hendisantika.springbootreactivewebmongodb.service;
+
+import id.my.hendisantika.springbootreactivewebmongodb.repository.CarRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Spring-Boot-Reactive-Web-MongoDB
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/5/24
+ * Time: 09:01
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor
+public class CarService {
+    private final CarRepository carRepository;
+
+
+}
